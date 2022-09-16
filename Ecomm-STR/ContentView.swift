6476023ddeb880f702ddd01487302ba86bfc0762
shreethaanu <shree96thaanu@gmail.com>
2022-09-17 00:44:07 +0530

    home page design added

--- Ecomm-STR/ContentView.swift
+++ Ecomm-STR/ContentView.swift
@@ -9,80 +9,96 @@ import SwiftUI
 import CoreData
 
 struct ContentView: View {
-    @Environment(\.managedObjectContext) private var viewContext
-
-    @FetchRequest(
-        sortDescriptors: [NSSortDescriptor(keyPath: \Item.timestamp, ascending: true)],
-        animation: .default)
-    private var items: FetchedResults<Item>
-
+    var headerButtons = ["All", "Men", "Women", "Kids"]
+    @State private var didTap:Bool = false
     var body: some View {
-        NavigationView {
-            List {
-                ForEach(items) { item in
-                    NavigationLink {
-                        Text("Item at \(item.timestamp!, formatter: itemFormatter)")
-                    } label: {
-                        Text(item.timestamp!, formatter: itemFormatter)
+        ScrollView(.vertical, showsIndicators: false) {
+            VStack {
+                topbar()
+                HStack {
+                    Text("Find Your Style")
+                        .multilineTextAlignment(.leading)
+                        .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+                        .padding(.leading)
+                    Spacer()
+                }
+                ScrollView(.horizontal) {
+                    HStack(spacing: 20) {
+                        ForEach(0..<headerButtons.count) { i in
+                            Button(headerButtons[i]) {
+                                print("Tapped")
+                                self.didTap = true
+                            }
+                                .font(.body)
+                                .frame(width: 80, height: 40)
+                                .border(.gray)
+                                .cornerRadius(5)
+                                .background(didTap ? Color.blue : Color.yellow)
+                        }
                     }
+                    .padding(.leading)
                 }
-                .onDelete(perform: deleteItems)
-            }
-            .toolbar {
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    EditButton()
-                }
-                ToolbarItem {
-                    Button(action: addItem) {
-                        Label("Add Item", systemImage: "plus")
+                .scrollIndicators(.hidden, axes: .horizontal)
+                
+                ScrollView(.horizontal, showsIndicators: false) {
+                    HStack(spacing: 16) {
+                        ForEach(0 ..< 5) { item in
+                            GeometryReader { geometry in
+                                RoundedRectangle(cornerRadius: 30)
+                                    .fill(LinearGradient(gradient: Gradient(colors: [Color(#colorLiteral(red: 0.2196078449, green: 0.007843137719, blue: 0.8549019694, alpha: 1)), Color(#colorLiteral(red: 0.5568627715, green: 0.3529411852, blue: 0.9686274529, alpha: 1))]), startPoint: .topLeading, endPoint: .bottomTrailing))
+                                    .rotation3DEffect(
+                                        Angle(
+                                            degrees: Double((geometry.frame(in: .global).minX - 20) / -20)
+                                        ),
+                                        axis: (x: 0, y: 1, z: 0),
+                                        anchor: .center,
+                                        anchorZ: 0.0,
+                                        perspective: 1.0
+                                    )
+                            }
+                            .frame(width: 300, height: 300)
+                        }
                     }
+                    .padding()
                 }
+                HStack(){
+                    Text("Most popular")
+                        .fontWeight(.heavy)
+                        .multilineTextAlignment(.leading)
+                        .font(/*@START_MENU_TOKEN@*/.body/*@END_MENU_TOKEN@*/)
+                        .padding(.leading)
+                    Spacer()
+                    Button("See all") {
+                        print("see all tapped")
+                    }
+                    .padding(.trailing)
+                    .foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
+                }
+                ScrollView(.horizontal, showsIndicators: false) {
+                    HStack(spacing: 16) {
+                        ForEach(0 ..< 10) { item in
+                            GeometryReader { geometry in
+                                RoundedRectangle(cornerRadius: 30)
+                                    .fill(LinearGradient(gradient: Gradient(colors: [Color(#colorLiteral(red: 0.3098039329, green: 0.01568627544, blue: 0.1294117719, alpha: 1)), Color(#colorLiteral(red: 0.8549019694, green: 0.250980407, blue: 0.4784313738, alpha: 1))]), startPoint: .topLeading, endPoint: .bottomTrailing))
+                            }
+                            .frame(width: 180, height: 300)
+                        }
+                    }
+                    .padding()
+                }
+                Spacer ()
             }
-            Text("Select an item")
+            
         }
     }
-
-    private func addItem() {
-        withAnimation {
-            let newItem = Item(context: viewContext)
-            newItem.timestamp = Date()
-
-            do {
-                try viewContext.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-            }
+    var bannerV: some View {
+        VStack{
+            
         }
     }
-
-    private func deleteItems(offsets: IndexSet) {
-        withAnimation {
-            offsets.map { items[$0] }.forEach(viewContext.delete)
-
-            do {
-                try viewContext.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-            }
-        }
     }
-}
-
-private let itemFormatter: DateFormatter = {
-    let formatter = DateFormatter()
-    formatter.dateStyle = .short
-    formatter.timeStyle = .medium
-    return formatter
-}()
-
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView().environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
+        ContentView()
     }
 }
---
+++ Ecomm-STR/SharedViews/topbar.swift
@@ -0,0 +1,50 @@
+//
+//  topbar.swift
+//  Ecomm-STR
+//
+//  Created by ShreeThaanu on 16/09/22.
+//
+
+import SwiftUI
+
+struct topbar: View {
+    var body: some View {
+        VStack {
+            HStack {
+                Button(action: {
+                    print("button pressed")
+                    
+                }) {
+                    Image("hamburger")
+                        .renderingMode(Image.TemplateRenderingMode?.init(Image.TemplateRenderingMode.original))
+                        .resizable(capInsets: EdgeInsets(top: 0.0, leading: 0.0, bottom: 0.0, trailing: 0.0))
+                        .frame(width: 25.0, height: 25.0)
+                }
+                .padding(.leading, 25.0)
+                Spacer()
+                
+                Text("STR ECOMM")
+                    .font(.headline)
+                    .fontWeight(.heavy)
+                Spacer()
+                Button(action: {
+                    print("button pressed")
+                }) {
+                    Image("cart")
+                        .renderingMode(Image.TemplateRenderingMode?.init(Image.TemplateRenderingMode.original))
+                        .resizable(capInsets: EdgeInsets(top: 0.0, leading: 0.0, bottom: 0.0, trailing: 0.0))
+                        .frame(width: 30.0, height: 30.0)
+                }
+                .padding(.trailing, 25.0)
+            }
+            .frame(maxWidth: .infinity, maxHeight: 150)
+            .background(/*@START_MENU_TOKEN@*//*@PLACEHOLDER=View@*/Color(hue: 1.0, saturation: 0.0, brightness: 0.904)/*@END_MENU_TOKEN@*/)
+        }
+    }
+}
+
+struct topbar_Previews: PreviewProvider {
+    static var previews: some View {
+        topbar()
+    }
+}
